8b8ea9781a4a89e570c1396361315d5ffcf5f672
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-12-05 13:37:33 -0500

    sslmode

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB,
-    sslmode:"require"
+    ssl: {
+      rejectUnauthorized: false
+    } 
 });
 
 app.use(bodyParser.json());
