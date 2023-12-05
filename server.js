10bcc53b6ac261ba23a193a1b939df608794e460
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-12-05 11:07:00 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = 8080;
+
+const pool = new Pool({
+    host: process.env.DB_SERVER,
+    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//todo init table
+// Create the todo table if it doesn't exist
+pool.query(`
+  CREATE TABLE IF NOT EXISTS todo (
+    id SERIAL PRIMARY KEY,
+    task VARCHAR(255) NOT NULL
+  )
+`)
+  .then(() => {
+    console.log('Todo table created or already exists');
+  })
+  .catch((error) => {
+    console.error('Error creating todo table:', error);
+  });
+
+// Serve HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// API endpoint to get all todo entries
+app.get('/api/todos', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM todo');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// API endpoint to add a new todo entry
+app.post('/api/todos', async (req, res) => {
+  const task = req.body.task;
+
+  try {
+    const result = await pool.query('INSERT INTO todo (task) VALUES ($1) RETURNING *', [task]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
