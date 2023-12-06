b93d8f44b2e68c7dc6faac3515de71e45ce25777
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-12-06 12:54:24 -0500

    remobved SSL bits:

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB,
-    ssl: {
-      rejectUnauthorized: false
-    } 
+
 });
 
 app.use(bodyParser.json());
